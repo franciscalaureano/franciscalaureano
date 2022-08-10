a8504439e6428e78bb8309e0eb597b156620e20c
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2022-08-10 16:29:15 +0100

    teste contacto

--- contact.php
+++ contact.php
@@ -44,29 +44,7 @@
   <!---------------------------------------->
   <h1 id="h1_contact">Contact Me</h1>
 
-  <?php
-
-        if (isset($_POST['submit'])){
-           // $name = $_POST['name'];
-            $email = $_POST['email'];
-            $subjectP = $_POST['subject'];
-            $description = $_POST['description'];
-
-            $to = "franciscarlaureano@gmail.com";
-
-            $subject = "Proposta Trabalho +" . $subjectP;
-            $message = "Message:" . $description;
-            $headers = "From:" . $email;
-            //$parameters= "Name:" . $name
-           
-           
-            
-
-            mail($to, $subject, $message, $headers );
-            //header("Location: sobre_nos.php?mailsend");
-            echo "<p style='color: #C25151'>Obrigada pelo e-mail :) Entrarei em contacto brevemente!";
-        }
-        ?>
+  
 
   <form method="post" action="contact.php">
     <label>Temos todo o gosto em falar contigo! Envia-nos o teu email:</label>
